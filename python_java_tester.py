8e86ef8fa9299d61f2de5e16dfbbeb2d75655fb2
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-13 23:59:44 -0800

    Update python_java_tester.py

--- python_java_tester.py
+++ python_java_tester.py
@@ -5,6 +5,6 @@ output = subprocess.run(['jshint', 'build/index.js'], capture_output=True)
 
 # check the exit code to see if there were errors
 if output.returncode == 0:
-    print("no errors found")
+    print("Great news! No errors found in index.js")
 else:
-    print("Ops, errors exist")
+    print("Ops, errors exist and code is not running correct")
